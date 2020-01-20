e0460b3b74030556f4e69ff8a98b3050937e2111
rstmsn <rosstitute@gmail.com>
2020-01-20 00:42:13 +0000

    Cleaned up in conjunction with PEP8

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pathlib import Path
 url_file = 'urls.json'
 previous_hash_file = 'previous.json'
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -14,6 +15,7 @@ class bcolors:
     WARNING = '\033[93m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
+
 
 def load_urls():
     try:
@@ -24,9 +26,14 @@ def load_urls():
     except UnicodeDecodeError:
         sys.exit('Invalid URL file detected. Exiting...')
     except json.decoder.JSONDecodeError:
-        sys.exit(f'{bcolors.HEADER}Malformed URL file detected. Add some URLS first...{bcolors.ENDC}')
+        sys.exit(
+            f'{bcolors.HEADER}' +
+            'Malformed URL file. Add some URLS first...' +
+            '{bcolors.ENDC}'
+        )
     else:
         return urls
+
 
 def load_previous_hashes():
     previous_hashes = {}
@@ -42,19 +49,22 @@ def load_previous_hashes():
     finally:
         return previous_hashes
 
+
 def hash_urls(urls):
     current_hashes = {}
     for page_url in urls:
         current_hashes[page_url] = hash_page_url(page_url)
     return current_hashes
 
+
 def hash_page_url(page_url):
     r = requests.get(page_url)
     m = hashlib.md5()
     m.update(r.content)
     page_url_hash = m.hexdigest()
-    
+
     return page_url_hash
+
 
 def check_for_changes(current_hashes, previous_hashes):
     detected_changes = False
@@ -67,16 +77,26 @@ def check_for_changes(current_hashes, previous_hashes):
             else:
                 print(f'\t{bcolors.OKBLUE}No Change{bcolors.ENDC}')
         else:
-            print(f'\t{bcolors.WARNING}Page checked for first time.{bcolors.ENDC}')
-    
+            print(
+                f'\t{bcolors.WARNING}' +
+                'Page checked for first time.' +
+                '{bcolors.ENDC}'
+            )
+
     if not detected_changes:
-            print(f'\n{bcolors.HEADER}No changes detected at this time.{bcolors.ENDC}')
+        print(
+            f'\n{bcolors.HEADER}' +
+            'No changes detected at this time.' +
+            '{bcolors.ENDC}'
+        )
+
 
 def write_current_hashes(current_hashes):
     with open(previous_hash_file, 'w') as outfile:
         json.dump(current_hashes, outfile)
 
-def main(): 
+
+def main():
     urls = load_urls()
     if urls:
         previous_hashes = load_previous_hashes()
@@ -84,6 +104,11 @@ def main():
         check_for_changes(current_hashes, previous_hashes)
         write_current_hashes(current_hashes)
     else:
-        print(f'{bcolors.WARNING}Add some URLS in urls.json first.{bcolors.ENDC}')
+        print(
+            f'{bcolors.WARNING}' +
+            'Add some URLS in urls.json first.' +
+            '{bcolors.ENDC}'
+        )
+
 
 main()
